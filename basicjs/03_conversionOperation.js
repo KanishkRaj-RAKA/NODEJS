dd6a7473a671cffeae7d6eaa51e6c6e66e271607
Kanishk Raj <119690882+KanishkRaj-RAKA@users.noreply.github.com>
2024-09-21 08:44:50 +0000

    03

--- basicjs/03_conversionOperation.js
+++ basicjs/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "78kani"
 
-// console.log(typeof score);  //string
+console.log(typeof score);  //string
 
 let valueInNumber = Number(score)
-// console.log(typeof valueInNumber); //NUmber
-// console.log(valueInNumber); // NaN
+console.log(typeof valueInNumber); //NUmber
+console.log(valueInNumber); // NaN
 
 
 //  "33" => 33
@@ -14,7 +14,7 @@ let valueInNumber = Number(score)
 let isLogined = "kanishk"
 
 let booleanIsLoggedIn = Boolean(isLogined)
-// console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
 
 // 1=> true; 0 => false
 // "" => false
@@ -24,8 +24,8 @@ let booleanIsLoggedIn = Boolean(isLogined)
 let someNumber = 33
 
 let stringNuber = String(someNumber)
-// console.log(stringNuber);
-// console.log(typeof stringNuber);
+console.log(stringNuber);
+console.log(typeof stringNuber);
 
 
 // ****************************** Operations  ****************************** 

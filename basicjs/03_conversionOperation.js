34d903e4d7e6c74ef5bf661f205d87bb27b443fb
Kanishk Raj <119690882+KanishkRaj-RAKA@users.noreply.github.com>
2024-09-21 08:23:32 +0000

    conversion of datatype

---
+++ basicjs/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "78kani"
+
+console.log(typeof score);  //string
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); //NUmber
+console.log(valueInNumber); // NaN
+
+
+//  "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLogined = "kanishk"
+
+let booleanIsLoggedIn = Boolean(isLogined)
+console.log(booleanIsLoggedIn);
+// 1=> true; 0 => false
+// "" => false
+// "kanishk" => true
+
+
+let someNumber = 33
+
+let stringNuber = String(someNumber)
+console.log(stringNuber);
+console.log(typeof stringNuber);
+
